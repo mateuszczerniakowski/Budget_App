4f660bd46807432606fba06bd85e0729ad99c5c7
mateuszczerniakowski <mattczerniakowski@gmail.com>
2022-04-25 12:37:23 +0200

    Add initial files

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+function Header (props){
+    return(
+       <div className=" container mb-4 my-4">
+              <h1 className="me-auto mb-4">Budget App</h1>
+              <button className="m-2 btn btn-primary btn-outline">Add Budget</button>
+              <button className="m-2 btn btn-outline-primary">Remove Budget</button>
+       </div>)}
+
+export default Header ;
